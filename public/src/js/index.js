41e59883021b8b7de40562d7b6d831f565809e4d
Ayoung Kim <ayoung.kim@lemonhc.com>
2022-10-14 10:01:44 +0900

    수정버튼 기능 구현

--- public/src/js/index.js
+++ public/src/js/index.js
@@ -1,5 +1,5 @@
 // step1 요구사항
-// TODO 메뉴 추가
+// FIXME 메뉴 추가
 // - [x] 메뉴의 이름을 입력받고 엔터키로 입력을 추가한다.
 // - [x] 메뉴의 이름을 입력받고 확인버튼으로 입력을 추가한다.
 // - [x] 추가되는 메뉴의 마크업 `<ul id="espresso-menu-list" class="mt-3 pl-0"><ul>` 안에 삽입해야 한다.
@@ -7,10 +7,14 @@
 // - [x] 메뉴가 추가되고 나면, input 은 빈 값으로 초기화한다.
 // - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
 
+// TODO 메뉴 수정
+// - [x] 메뉴의 수정 버튼 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
+// - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  // form 태그가 자동으로 전송되는 것을 막아준다. submit: form 이 전손되는 이벤트 명
+  // form 태그가 자동으로 전송되는 것을 막아준다. submit: form 이 전송되는 이벤트 명
   $("#espresso-menu-form").addEventListener("submit", (e) => {
     e.preventDefault();
   })
@@ -45,21 +49,29 @@ function App() {
     $("#espresso-menu-name").value = '';
   }
 
+  // 확인버튼
   $("#espresso-menu-submit-button").addEventListener("click", (e) => {
     addMenuName();
   })
 
-  // 메뉴의 이름을 받는 건
+  // 엔터키
   $("#espresso-menu-name").addEventListener("keypress", (e) => {
     addMenuName();
+  })
+
+  // 수정버튼
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    // console.log(e.target);
+    if (e.target.classList.contains("menu-edit-button")) {
+      const $menuName = e.target.closest("li").querySelector(".menu-name");
+      const updateMenuName = prompt("메뉴명을 수정해 주세요", $menuName.innerText);
+      e.target.closest("li").querySelector(".menu-name").innerText = updateMenuName;ㅊ
+    }
   })
 }
 
 App();
 
-// TODO 메뉴 수정
-// - [] 메뉴의 수정 버튼 이벤트를 받고, 메뉴 수정하는 모달창이 뜬다.
-// - [] 모달탕에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
 
 // TODO 메뉴 삭제
 // - [] 메뉴 삭제 버튼 클릭 이벤트를 받고, 메뉴 삭제 컨펌 모달창이 뜬다.
